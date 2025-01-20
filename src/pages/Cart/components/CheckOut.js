cd309b97d1cc314fa1a67ae94699004fd30c50a5
Chinthana K <chinthanakumar1999@gmail.com>
2025-01-20 18:33:54 +0530

    Variables required issues fixed

--- src/pages/Cart/components/CheckOut.js
+++ src/pages/Cart/components/CheckOut.js
@@ -10,11 +10,8 @@ export const Checkout = ({setCheckout}) => {
     const [user, setUser] = useState({});
 
     const navigate = useNavigate();
-
-    const token = JSON.parse(sessionStorage.getItem("token"));
-    const cbid = JSON.parse(sessionStorage.getItem("cbid"));
     
-   useEffect(() => {
+    useEffect(() => {
   
     async function fetchData() {
         try{
